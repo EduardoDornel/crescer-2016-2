edf6fea65cb350db9cffc0fda1443ace147e37ef
Unknown <eduardo.dornel@hotmail.com>
2016-09-28 16:46:32 -0300

    test(DwarfTest): testes de mudança do status vivo/morto do dwarf

--- src/modulo-01-java-OO/modulo-01/DwarvesTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarvesTest.java
@@ -21,9 +21,30 @@ public class DwarvesTest
         
         for(int i = 0; 11 > i; i++)
             dwarf.perdeVida();
-
-        
+            
         assertEquals(0, dwarf.getVida());
+    }
+    
+    @Test
+    public void dwarfMorreu(){
+        Dwarves dwarf = new Dwarves();
+        
+        for(int i = 0; 15 > i; i++)
+            dwarf.perdeVida();
+            
+        assertEquals(0, dwarf.getVida());
+        assertFalse(dwarf.isVivo());
+    }
+    
+    @Test
+    public void dwarfPerdeVida5Vezes(){
+        Dwarves dwarf = new Dwarves();
+        
+        for(int i = 0; 5 > i; i++)
+            dwarf.perdeVida();
+            
+        assertEquals(60, dwarf.getVida());
+        assertTrue(dwarf.isVivo());
     }
 }
 
