cc55efc87ab94b009286d015f29be98beddb4388
Unknown <eduardo.dornel@hotmail.com>
2016-09-28 15:46:14 -0300

    refactor(Dwarves) removendo constrtutor e linhas desnecessárias

--- src/modulo-01-java-OO/modulo-01/Dwarves.java
+++ src/modulo-01-java-OO/modulo-01/Dwarves.java
@@ -1,17 +1,11 @@
 public class Dwarves{
-    private int vida;
+    private int vida = 110;
     
-    public Dwarves(){
-        vida = 110;
-    }
-    
-    public void setVida(int vida){
-        this.vida = vida;
-    }
+    public void perdeVida(){
+        vida -=10;
+    }   
     
     public int getVida(){
         return vida;
     }
-    
-    
 }
