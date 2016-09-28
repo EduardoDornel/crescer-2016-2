b0d7bf58daab22ffca5bf6cf2d3006b61aea3b62
Unknown <eduardo.dornel@hotmail.com>
2016-09-28 16:47:59 -0300

    feat(Dwarf): criando atributo, método isVivo() e alterando o perdeVida()

--- src/modulo-01-java-OO/modulo-01/Dwarves.java
+++ src/modulo-01-java-OO/modulo-01/Dwarves.java
@@ -1,11 +1,19 @@
 public class Dwarves{
     private int vida = 110;
+    private boolean vivo = true;
     
     public void perdeVida(){
-        vida -=10;
+        if(vida >= 10)
+            vida -=10;
+        else
+            vivo = false;
     }   
     
     public int getVida(){
         return vida;
+    }
+    
+    public boolean isVivo(){
+        return vivo;
     }
 }
