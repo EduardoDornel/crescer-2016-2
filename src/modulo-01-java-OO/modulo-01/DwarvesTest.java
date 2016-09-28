e10ac206232cd070fda54a2eff2e28d1e3bee089
Unknown <eduardo.dornel@hotmail.com>
2016-09-28 15:47:39 -0300

    test(Dwarves) adicionando novos testes

--- src/modulo-01-java-OO/modulo-01/DwarvesTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarvesTest.java
@@ -9,9 +9,21 @@ public class DwarvesTest
 {
   
     @Test
-    public void VerificarVidaDwarf(){
+    public void criandoDwarf(){
         Dwarves dwarf = new Dwarves();
         
         assertEquals(110, dwarf.getVida());
     }
+    
+    @Test
+    public void dwarfPerdeVida11Vezes(){
+        Dwarves dwarf = new Dwarves();
+        
+        for(int i = 0; 11 > i; i++)
+            dwarf.perdeVida();
+
+        
+        assertEquals(0, dwarf.getVida());
+    }
 }
+
