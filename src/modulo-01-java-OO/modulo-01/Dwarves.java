af49be5312015ab53fce906e9721493b24499f16
Unknown <eduardo.dornel@hotmail.com>
2016-09-29 15:39:25 -0300

    refactor(Dwarves): melhorando método getNumeroSorte

--- src/modulo-01-java-OO/modulo-01/Dwarves.java
+++ src/modulo-01-java-OO/modulo-01/Dwarves.java
@@ -4,36 +4,39 @@ public class Dwarves{
     private DataTerceiraEra dataNascimento;
     private int experiencia;
     
+    public Dwarves(){
+        dataNascimento = new DataTerceiraEra(1, 1, 1);
+    }
+    
     public Dwarves(String nome, DataTerceiraEra dataNascimento){
         this.nome = nome;
         this.dataNascimento = dataNascimento;
     }
     
-    public Dwarves(){
-        dataNascimento = new DataTerceiraEra(1, 1, 1);
-    }
-    
     public double getNumeroSorte(){
         double numeroSorte = 101.0;
         if(dataNascimento.ehBissexto() && vida >= 80 && vida <= 90)
-            return numeroSorte * -33;
-        else if(nome.equals("Seixas") || nome.equals("Meireles")){
-            numeroSorte *= 33;
-            return numeroSorte % 100;
-        }        
+            numeroSorte *= -33.0;
+        else if(!dataNascimento.ehBissexto() && ("Seixas".equals(this.nome) || "Meireles".equals(this.nome))){
+            numeroSorte = numeroSorte * 33 % 100;
+        }       
         return numeroSorte;
     }
     
     public void perdeVida(){
-        vida -=10;
+        double numero = this.getNumeroSorte();
+        if(numero < 0)
+            experiencia += 2;
+        if(numero > 100)
+            vida -=10;
     }   
     
     public int getExperiencia(){
         return experiencia;
     }
     
-    public void setExperiencia(int experiencia){
-        this.experiencia = experiencia;
+    public DataTerceiraEra getDataNascimento(){
+        return dataNascimento;
     }
     
     public int getVida(){
