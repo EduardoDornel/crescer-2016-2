376a2d06922be9ef56d5450021e026f941caec28
Eduardo <eduardo.dornel@hotmail.com>
2016-09-29 02:39:59 -0300

    feat(dwarves): adicionar novas funcionalidades

--- src/modulo-01-java-OO/modulo-01/Dwarves.java
+++ src/modulo-01-java-OO/modulo-01/Dwarves.java
@@ -1,19 +1,43 @@
 public class Dwarves{
+    private String nome;
     private int vida = 110;
-    private boolean vivo = true;
+    private DataTerceiraEra dataNascimento;
+    private int experiencia;
+    
+    public Dwarves(String nome, DataTerceiraEra dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+    
+    public Dwarves(){
+        dataNascimento = new DataTerceiraEra(1, 1, 1);
+    }
+    
+    public double getNumeroSorte(){
+        double numeroSorte = 101.0;
+        if(dataNascimento.ehBissexto() && vida >= 80 && vida <= 90)
+            return numeroSorte * -33;
+        else if(nome.equals("Seixas") || nome.equals("Meireles")){
+            numeroSorte *= 33;
+            return numeroSorte % 100;
+        }        
+        return numeroSorte;
+    }
     
     public void perdeVida(){
-        if(vida >= 10)
-            vida -=10;
-        else
-            vivo = false;
+        vida -=10;
     }   
+    
+    public int getExperiencia(){
+        return experiencia;
+    }
+    
+    public void setExperiencia(int experiencia){
+        this.experiencia = experiencia;
+    }
     
     public int getVida(){
         return vida;
     }
     
-    public boolean isVivo(){
-        return vivo;
-    }
 }
