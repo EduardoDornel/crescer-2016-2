cc321bd3eac2b9d6e552a2e6913edfb21d132858
Unknown <eduardo.dornel@hotmail.com>
2016-09-29 15:38:41 -0300

    test(Dwarves.test): adicionando novos testes aos Dwarves

--- src/modulo-01-java-OO/modulo-01/DwarvesTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarvesTest.java
@@ -34,5 +34,38 @@ public class DwarvesTest
             
         assertEquals(60, dwarf.getVida());
     }
+    
+    @Test
+    public void dwarfNasceComDataNascimento(){
+        Dwarves dwarf = new Dwarves("", new DataTerceiraEra(1, 1, 2010));
+        
+        assertEquals(1, dwarf.getDataNascimento().getDia());
+        assertEquals(1, dwarf.getDataNascimento().getMes());
+        assertEquals(2010, dwarf.getDataNascimento().getAno());
+    }
+    
+    @Test
+    public void numeroSorteRetorna101(){
+         Dwarves dwarf = new Dwarves("", new DataTerceiraEra(1, 1, 2010));
+         
+         assertEquals(101.0, dwarf.getNumeroSorte(), 0.001);
+    }
+    
+    @Test
+    public void umeroSorteRetorna33(){
+         Dwarves dwarf = new Dwarves("Seixas", new DataTerceiraEra(1, 1, 2015));
+         
+         assertEquals(33.0, dwarf.getNumeroSorte(), 0.001);
+    }
+    
+    @Test
+    public void numeroSorteRetorna3333Negativo(){
+        Dwarves dwarf = new Dwarves("Seixas", new DataTerceiraEra(1, 1, 2016));
+        
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        
+        assertEquals(-3333.0, dwarf.getNumeroSorte(), 0.001);
+    }
 }
 
