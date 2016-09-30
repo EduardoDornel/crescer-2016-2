8810c999c052a2498eeac4570c7cd288381378c2
Eduardo <eduardo.dornel@hotmail.com>
2016-09-30 09:57:36 -0300

    feat(Inventario): criação de métodos getDescricaoItens e itemMaiorQtd

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -7,11 +7,36 @@ public class Inventario{
         return itens.get(index);
     }
     
+    public ArrayList getItens(){
+        return itens;
+    }
+    
     public void adicionarItem(Item item){
         itens.add(item);
     }
     
     public void removerItem(Item item){
         itens.remove(item);
+    }
+    
+    public String getDescricoesItens(){
+        String descricoes = "";
+        for(int i = 0; itens.size() > i; i++){
+            descricoes += itens.get(i).getDescricao();
+            if(i + 1 == itens.size())
+                descricoes += ".";
+            else
+                descricoes += ", ";
+        }
+        return descricoes;
+    }
+    
+    public Item itemMaiorQuantidade(){
+        Item maiorQuantidade = new Item("",0);
+        for(int i = 0; itens.size() > i; i++){
+            if(itens.get(i).getQuantidade() > maiorQuantidade.getQuantidade())
+                maiorQuantidade = itens.get(i);
+        }
+        return maiorQuantidade;
     }
 }
