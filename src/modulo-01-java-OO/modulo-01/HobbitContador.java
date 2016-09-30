d68a617f44375c4992dc3975bdf32d44b17d47f9
Unknown <eduardo.dornel@hotmail.com>
2016-09-30 17:22:32 -0300

    feat(HobbitContador): criar classe de hobbitContadores(em construcao)

---
+++ src/modulo-01-java-OO/modulo-01/HobbitContador.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HobbitContador{
+    ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+    
+    public int calculaDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
+       //arrayDePares.add(new ArrayList<>(Arrays.asList(15, 18)));
+       //arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
+       //arrayDePares.add(new ArrayList<>(Arrays.asList(12, 60)));
+       //IMPLEMENTAR ESTA CLASSE DEPOIS
+       return 0;
+    }
+}
