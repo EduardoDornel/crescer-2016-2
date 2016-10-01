c270afdb7ecc5b8b255d2df5fc2b8d8471f1b053
Eduardo <eduardo.dornel@hotmail.com>
2016-10-01 18:38:36 -0300

    feat(InventarioTest): criar novos cenários de testes

--- src/modulo-01-java-OO/modulo-01/InventarioTest.java
+++ src/modulo-01-java-OO/modulo-01/InventarioTest.java
@@ -28,6 +28,21 @@ public class InventarioTest
     }
     
     @Test
+    public void adicionarCincoItensNoInventario() {
+        Inventario inventario = new Inventario();
+        Item espadaZ = new Item("Espada Z", 1);
+        Item item = new Item("Iron Boots", 1);
+        inventario.adicionarItem(espadaZ);
+        inventario.adicionarItem(item);
+        inventario.adicionarItem(item);
+        inventario.adicionarItem(item);
+        inventario.adicionarItem(item);
+        assertTrue(inventario.getItens().contains(espadaZ));
+        assertTrue(inventario.getItens().contains(item));
+        assertEquals(5, inventario.getItens().size());
+    }
+    
+    @Test
     public void removeItem(){
         Inventario inventario = new Inventario();
         Item pistola = new Item("pistola", 3);
@@ -39,6 +54,13 @@ public class InventarioTest
         Item bomba = new Item("bomba", 10);
         inventario.adicionarItem(bomba);
         assertEquals(bomba, inventario.getItens().get(0));
+    }
+    
+    @Test
+    public void inventarioSemAdicionarItem() {
+        Inventario inventario = new Inventario();
+        Item item = new Item("Espada Z", 1);
+        assertFalse(inventario.getItens().contains(item));
     }
     
     @Test
@@ -68,5 +90,63 @@ public class InventarioTest
         assertEquals(metralhadora, inventario.itemMaiorQuantidade());
     }
     
+    @Test
+    public void adicionarDoisItensERemoverUm() {
+        Inventario inventario = new Inventario();
+        Item item = new Item("item", 1);
+        Item item2 = new Item("item2", 1);
+        inventario.adicionarItem(item);
+        inventario.adicionarItem(item2);
+        inventario.removerItem(item);
+        assertFalse(inventario.getItens().contains(item));
+        assertTrue(inventario.getItens().contains(item2));
+    }
     
+    @Test
+    public void removerItemSemTerAdicionalo(){
+        Inventario inventario = new Inventario();
+        Item item = new Item("item", 1);
+        inventario.removerItem(item);
+        assertFalse(inventario.getItens().contains(item));
+    }    
+    
+    @Test
+    public void getDescricoesItensComVariosItens() {
+        Inventario inventario = new Inventario();
+        inventario.adicionarItem(new Item("Adaga", 14));
+        inventario.adicionarItem(new Item("Escudo", 2));
+        inventario.adicionarItem(new Item("Bracelete", 56));
+        assertEquals("Adaga, Escudo, Bracelete", inventario.getDescricoesItens());
+    }
+    
+    @Test
+    public void getDescricoesItensComApenasUmItem() {
+        Inventario inventario = new Inventario();
+        inventario.adicionarItem(new Item("Bracelete", 30));
+        assertEquals("Bracelete", inventario.getDescricoesItens());
+    }
+    
+    @Test
+    public void getDescricoesItensSemItens() {
+        Inventario inventario = new Inventario();
+        assertEquals("", inventario.getDescricoesItens());
+    }
+    
+    @Test
+    public void aumentarUnidadeSemItens() {
+        Inventario inventario = new Inventario();
+        inventario.aumentarUnidadesDosItens(1000);
+        assertEquals(0, inventario.getItens().size());
+    }
+    
+    @Test
+    public void itemComMaiorQuantidadeCom3ItensDeQuantidadesIguais() {
+        Inventario inventario = new Inventario();
+        inventario.adicionarItem(new Item("Espada de aço", 2));
+        inventario.adicionarItem(new Item("Poção polissuco", 2));
+        inventario.adicionarItem(new Item("Lucky egg", 2));
+        Item item = inventario.itemMaiorQuantidade();
+        assertEquals("Espada de aço", item.getDescricao());
+        assertEquals(2, item.getQuantidade());
+    }
 }
