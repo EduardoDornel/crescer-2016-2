06c0586becab82c200fa19d0b6b62c23f8a4eec2
Unknown <eduardo.dornel@hotmail.com>
2016-10-03 17:14:19 -0300

    feat(Personagem): nova classe pai -> Elfos e dwarves vão herdar ela

---
+++ src/modulo-01-java-OO/modulo-01/Personagem.java
@@ -0,0 +1,35 @@
+public class Personagem{
+    protected String nome;
+    protected int experiencia, vida;
+    protected Status status;
+    protected Inventario inventario = new Inventario();
+    
+    public Personagem(String nome){
+        this.nome = nome;
+        this.status = Status.VIVO;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public Inventario getInventario(){
+        return inventario;
+    }
+    
+    public int getExperiencia(){
+        return experiencia;
+    }
+    
+    public Status getStatus(){
+        return status;
+    }
+    
+    public int getVida(){
+        return vida;
+    }
+}
