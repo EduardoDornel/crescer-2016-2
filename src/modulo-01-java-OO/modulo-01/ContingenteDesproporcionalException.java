02176f305ad85d3f27a63ff16cfcc246ba02af66
Eduardo <eduardo.dornel@hotmail.com>
2016-10-08 17:28:04 -0300

    feat(ContingenteDesproporcional): criar exception para contingente desproporcional

---
+++ src/modulo-01-java-OO/modulo-01/ContingenteDesproporcionalException.java
@@ -0,0 +1,6 @@
+public class ContingenteDesproporcionalException extends Exception{
+    
+    public ContingenteDesproporcionalException(){
+        super("Não foi possivel dividir em partes iguais os elfos para o ataque");
+    }
+}
