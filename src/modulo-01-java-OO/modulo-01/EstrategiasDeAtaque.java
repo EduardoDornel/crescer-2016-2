432cbff04fb2ea8541a1be6692ac0e54391a4450
Eduardo <eduardo.dornel@hotmail.com>
2016-10-10 09:44:17 -0300

    feat(estrategiasAtaque): adicionar novo método para interface

--- src/modulo-01-java-OO/modulo-01/EstrategiasDeAtaque.java
+++ src/modulo-01-java-OO/modulo-01/EstrategiasDeAtaque.java
@@ -4,4 +4,5 @@ public interface EstrategiasDeAtaque
 {
     List<Elfo> getOrdemDeAtaque(List<Elfo> atacantes)throws ContingenteDesproporcionalException;
     List<Elfo> ataqueIntercalado(List<Elfo> atacantes)throws ContingenteDesproporcionalException;
+    List<Elfo> ataqueLittleMumu(List<Elfo> atacantes)throws ContingenteDesproporcionalException;
 }
