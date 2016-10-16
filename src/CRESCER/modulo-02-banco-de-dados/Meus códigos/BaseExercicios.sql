b16dd1075d39855755b33f0ce36b9afea2379a94
Eduardo <eduardo.dornel@hotmail.com>
2016-10-16 20:45:47 -0200

    feat(exercicios bd): adicionar novos exercicios

--- src/CRESCER/modulo-02-banco-de-dados/Meus códigos/BaseExercicios.sql
+++ src/CRESCER/modulo-02-banco-de-dados/Meus códigos/BaseExercicios.sql
@@ -10,10 +10,9 @@ select count(1) as pedidosSetembro2016
 from Pedido
 where dataPedido between convert(datetime, '2016/09/01', 101) and convert(datetime, '2016/09/30', 101)
 
-/*ERRO
-select pro.nome as nomeProduto
-from Produto pro, ProdutoMaterial prodM
-INNER JOIN Material mat ON mat.IDMaterial = prodM.IDProduto*/
+select Nome
+from Produto pro
+INNER JOIN ProdutoMaterial prodM ON prodM.IDMaterial = 15836 and prodM.IDProduto = pro.IDProduto
 
 select nome 
 from Cliente
@@ -22,4 +21,20 @@ where RazaoSocial LIKE '%LTDA' or nome LIKE '%LTDA'
 Insert into Produto(Nome, precoCusto, PrecoVenda, Situacao)
 values('Galocha Maragato', 35.67, 77.95, 'A')
 
+--5
+/*select pro.nome
+from Produto pro
+LEFT OUTER JOIN PedidoItem pedItem ON pro.IDProduto = pedItem.IDProduto */
 
+--6(Não funciona)
+select top 1 UF , count(*)as contaUf
+from Cidade cid
+INNER JOIN Cliente cli ON cli.IDCidade = cid.IDCidade
+group by UF
+order by contaUf desc
+
+select IDPedido, dataEntrega
+from  Pedido ped
+INNER JOIN ProdutoMaterial prodM ON prodM.IDProduto = PedidoItem.IDProduto  
+where prodM.IDMaterial = 14650 or prodM.IDMaterial = 15703 or prodM.IDMaterial = 17226 or
+	prodM.IDMaterial = 15836 or prodM.IDMaterial = 16003 or prodM.IDMaterial = 16604
--- src/CRESCER/modulo-02-banco-de-dados/Meus códigos/TAREFA 3 - exercicios 7-10.sql
+++ src/CRESCER/modulo-02-banco-de-dados/Meus códigos/TAREFA 3 - exercicios 7-10.sql
@@ -1,7 +1,33 @@
-select * from Empregado
-select * from Departamento
+select nomeEmpregado, DATEDIFF(MONTH, DataAdmissao, getDate()) as mesesTrabalhados
+from Empregado
+where DataAdmissao BETWEEN CONVERT(datetime, '01/05/1980', 103) and CONVERT(datetime, '20/01/1982', 103);
 
-select emp.nomeEmpregado as nomeEmpregado,
+select top 1 WITH TIES Cargo,
+       count(1) as total 
+from Empregado
+group by Cargo
+order by total desc 
+
+select UF, count(1) as qtdCidades
+ from Cidade
+ group by UF
+
+ select nome, count(*) as nomesRepetidos
+ from Cidade
+  group by Nome, UF
+  having count(*) > 1
+
+  select isnull(max (IDAssociado), 0) + 1 as proximoId
+  from Associado
+
+  select NomeEmpregado, salario,
+  case when salario > 2326.0 then 27.5
+	   when salario < 1164.0 then 0
+	   else  15
+	   end percentualIr
+  from Empregado
+
+  select emp.nomeEmpregado as nomeEmpregado,
 	   dep.NomeDepartamento as empregadoDepartamento,
 	   ger.nomeEmpregado as nomeGerente,
 	   depGer.NomeDepartamento as gerenteDepartamento
