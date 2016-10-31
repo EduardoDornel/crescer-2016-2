8ab8edf30a01a518e6ccf89bf5c384c3b0faf1f2
eduardo.ribas <eduardo.dornel@hotmail.com>
2016-10-31 17:28:34 -0200

    feat(streetFighter): iniciar exercicio street fighter

---
+++ src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class FichaTecnicaModel
+    {
+        string PrimeiraAparicao =  "Street Fighter II The World Warrior(1991).";
+        DateTime Nascimento = new DateTime(12, 2, 1966);
+        int Altura = 192;
+        int Peso = 96;
+        string Medidas = "B198, C120, Q172.";
+        string TipoSanguineo = "B.";
+        string HabilidadesEspeciais = "Cacar, Eletricidade.";
+        string Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
+        string Desgosta = "Army ants(espécie de formiga).";
+        string EstiloDeLuta = "Luta Selvagem autodidata(Army Ants) / Capoeira.";
+        string Origem = "Brasil(lugar de nascença é provável como sendo Tailândia).";
+        string UmaFalaDeVitoria = "\"Ver você em ação é uma piada!\"";
+        string SSF2Nickname = "\"A selvagem criança da natureza.\"";
+        string SFA3Nickname = "\"A animal pessoa amazônica.\"";
+        string SF4Nickname = "\"Guerreiro da selva\"";
+        string SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil(ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?).";
+        string SF2Stage = "Bacia do rio Amazonas(Brasil).";
+        string GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack.";
+    }
+}
