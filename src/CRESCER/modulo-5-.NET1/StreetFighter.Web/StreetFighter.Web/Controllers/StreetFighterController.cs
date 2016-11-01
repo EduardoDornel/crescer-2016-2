b00628a68fbab97dabcea6834261b8bb08f46fc1
Eduardo <eduardo.dornel@hotmail.com>
2016-11-01 09:25:01 -0200

    feat(StreetFighter.Web): finalizar site StreetFighter

--- src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
+++ src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StreetFighter.Web.Models;
 
 namespace StreetFighter.Web.Controllers
 {
@@ -15,7 +16,38 @@ namespace StreetFighter.Web.Controllers
 
         public ActionResult FichaTecnica()
         {
-            return View("FichaTecnicaModel");
+            var FichaTec = new FichaTecnicaModel();
+            FichaTec.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
+            FichaTec.DataNascimento = Convert.ToDateTime("12/02/1966");
+            FichaTec.Altura = 192;
+            FichaTec.Peso = 96;
+            FichaTec.Medidas = "B198, C120, Q172";
+            FichaTec.TipoSanguineo = "B";
+            FichaTec.HabilidadesEspeciais = "Caçar, Eletricidade";
+            FichaTec.Gosta = "Frutas tropicais, Pirarucu, Sua mãe";
+            FichaTec.Desgosta = "Army ants (espécie de formiga)";
+            FichaTec.EstiloDeLuta = "Luta Selvagem autodidata (ArmyAnts) / Capoeira";
+            FichaTec.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia)";
+            FichaTec.FalaDeVitória = "\"Ver você em ação é uma piada!\"";
+            FichaTec.SSF2Nickname = "\"A selvagem criança da natureza\"";
+            FichaTec.SFA3Nickname = "\"A animal pessoa amazônica\"";
+            FichaTec.SF4Nickname = "\"Guerreiro da selva\"";
+            FichaTec.SFA3Stage = "Ramificação do Rio Madeira -pantano, Brasil(ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
+            FichaTec.SF2Stage = "Bacia do rio Amazonas (Brasil).";
+            FichaTec.GolpesEspeciais = "Electric Thunder,Rolling Attack.";
+            return View(FichaTec);
+        }
+
+        public ActionResult Sobre()
+        {
+            var SobreMim = new SobreModel();
+            SobreMim.Nome = "Eduardo Dornel Ribas";
+            SobreMim.Idade = 19;
+            SobreMim.Telefone = "(51)9273-4285";
+            SobreMim.NaturalDe = "Gravataí";
+            SobreMim.GitHub = "https://github.com/EduardoDornel";
+            SobreMim.Historia = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            return View(SobreMim);
         }
     }
 }
--- src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
+++ src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -7,23 +7,23 @@ namespace StreetFighter.Web.Models
 {
     public class FichaTecnicaModel
     {
-        string PrimeiraAparicao =  "Street Fighter II The World Warrior(1991).";
-        DateTime Nascimento = new DateTime(12, 2, 1966);
-        int Altura = 192;
-        int Peso = 96;
-        string Medidas = "B198, C120, Q172.";
-        string TipoSanguineo = "B.";
-        string HabilidadesEspeciais = "Cacar, Eletricidade.";
-        string Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
-        string Desgosta = "Army ants(espécie de formiga).";
-        string EstiloDeLuta = "Luta Selvagem autodidata(Army Ants) / Capoeira.";
-        string Origem = "Brasil(lugar de nascença é provável como sendo Tailândia).";
-        string UmaFalaDeVitoria = "\"Ver você em ação é uma piada!\"";
-        string SSF2Nickname = "\"A selvagem criança da natureza.\"";
-        string SFA3Nickname = "\"A animal pessoa amazônica.\"";
-        string SF4Nickname = "\"Guerreiro da selva\"";
-        string SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil(ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?).";
-        string SF2Stage = "Bacia do rio Amazonas(Brasil).";
-        string GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack.";
+        public string PrimeiraAparicao { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Altura { get; set; }
+        public int Peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string HabilidadesEspeciais { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloDeLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaDeVitória { get; set; }
+        public string SSF2Nickname { get; set; }
+        public string SFA3Nickname { get; set; }
+        public string SF4Nickname { get; set; }
+        public string SFA3Stage { get; set; }
+        public string SF2Stage { get; set; }
+        public string GolpesEspeciais { get; set; }
     }
 }
