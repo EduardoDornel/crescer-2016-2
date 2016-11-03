b37504257d301d192f322cd0b17c3f5b051a840c
Unknown <eduardo.dornel@hotmail.com>
2016-11-03 13:18:48 -0200

    feat(StreetFigther.Web): voltar commit pq no anterior fiz besteira

---
+++ src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class FichaTecnicaModel
+    {
+        public string Nome { get; set; }
+        public string Imagem { get; set;}
+        public string PrimeiraAparicao { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Altura { get; set; }
+        public decimal Peso { get; set; }
+        public string Origem { get; set; } 
+        public string GolpesEspeciais { get; set; }
+        public bool PersonagemOculto { get; set; }
+    }
+}
