d9dd6d63826621817f46456ae6f4f149ffdb0b06
Unknown <eduardo.dornel@hotmail.com>
2016-11-08 17:22:35 -0200

    ..

---
+++ src/CRESCER/modulo-5-.NET1/StreetFighter.Web/StreetFighter.RespositorioEF/PersonagensEf.sql
@@ -0,0 +1,5 @@
+select * from Personagens
+
+INSERT into Personagens(Nome, Origem, GolpesEspeciais, DataNascimento, PrimeiraAparicao, Peso, Imagem, PersonagemOculto, Altura)
+VALUES('Eduardo','Brasil','Hackear','12/02/1997','Hospital',65,null,1,180)
+
