eb98237aded8c321ec73b39b81039a53dce49ce9
eduardo.ribas <eduardo.dornel@hotmail.com>
2016-11-16 14:48:02 -0200

    feat(evento2): iniciar projeto

---
+++ src/CRESCER/modulo-07-.NET2/Projeto2Evento/Projeto2Evento.Dominio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2Evento.Dominio
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+
+        }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Documento { get; set;}
+    }
+}
---
+++ src/CRESCER/modulo-5-.NET1/ForcaDaIlha3/ForcaDaIlha3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForcaDaIlha3.WebApiApplication" Language="C#" %>
