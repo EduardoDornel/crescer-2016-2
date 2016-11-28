c2915d3ec65783a122542b4a790d3339207c576b
Eduardo <eduardo.dornel@hotmail.com>
2016-11-28 09:49:37 -0200

    refactor(MeuFileUtils): colocar verificação de extensão em um método

--- src/CRESCER/modulo-08-java/aula2/src/main/java/br/com/cwi/crescer/aula2/MeuFileUtils.java
+++ src/CRESCER/modulo-08-java/aula2/src/main/java/br/com/cwi/crescer/aula2/MeuFileUtils.java
@@ -7,20 +7,45 @@ package br.com.cwi.crescer.aula2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
  * @author Eduardo
  */
 public class MeuFileUtils {
-
-    public void mk(String nome) {
-        String extensao = null;
-        try{
-            extensao = nome.substring(nome.lastIndexOf("."), nome.length());
-        }catch(Exception e){
+        
+    public MeuFileUtils(String metodo){
+        Scanner s = new Scanner(System.in);
+        switch(metodo){
+            case "mk":
+                System.out.println("Digite o caminho do arquivo/pasta que deseja criar!");
+                mk(s.nextLine());
+                break;
+            case "rm":
+                System.out.println("Digite o caminho do arquivo que deseja excluir");
+                rm(s.nextLine());
+                break;
+            case "ls":
+                System.out.println("Digite o nome do arquivo que deseja saber o caminho absoluto ou da pasta que deseja conhecer seus itens");
+                ls(s.nextLine());
+                break;
+            case "mv":
+                System.out.println("Digite o caminho atual do arquivo");
+                String antigoCaminho = s.nextLine();
+                System.out.println("Digite o novo caminho do arquivo");
+                String novoCaminho = s.nextLine();
+                mv(antigoCaminho, novoCaminho);
+                break;
+            default:
+                System.out.println("Você não digitou um comando existente.");
         }
-            if (extensao == null) {
+        s.close();
+    }
+
+    private void mk(String nome) {
+        String extensao = verificaExtensao(nome);  
+        if (extensao == null) {
             final File diretorio = new File(nome);
             diretorio.mkdirs();
         } else{ 
@@ -33,12 +58,8 @@ public class MeuFileUtils {
         }
     }
 
-    public void rm(String nome) {
-        String extensao = null;
-        try{
-            extensao = nome.substring(nome.lastIndexOf("."), nome.length());
-        }catch(Exception e){
-        }
+    private void rm(String nome) {
+        String extensao = verificaExtensao(nome);  
         if (extensao == null) {
             System.out.println("Arquivo inválido");
         } else {
@@ -46,13 +67,9 @@ public class MeuFileUtils {
         }
     }
 
-    public void ls(String nome) {
-        String extensao = null;
-        File file = new File(nome);       
-        try{
-            extensao = nome.substring(nome.lastIndexOf("."), nome.length());
-        }catch(Exception e){
-        }      
+    private void ls(String nome) {
+        String extensao = verificaExtensao(nome);  
+        File file = new File(nome);             
         if (extensao == null) {
             final File[] list = file.listFiles();
             for (File f : list) {
@@ -63,12 +80,8 @@ public class MeuFileUtils {
         }
     }
 
-    public void mv(String nome, String nomeNovo) {
-        String extensao = null;
-        try{
-            extensao = nome.substring(nome.lastIndexOf("."), nome.length());
-        }catch(Exception e){
-        }     
+    private void mv(String nome, String nomeNovo) {
+        String extensao = verificaExtensao(nome);  
         if (extensao == null) {
             System.out.println("Arquivo inválido");
         } else {
@@ -77,5 +90,14 @@ public class MeuFileUtils {
                 System.out.println("Arquivo movido com sucesso");
             }
         }
+    }
+    
+    private String verificaExtensao(String arquivo){
+        String extensao = null;
+        try{
+            extensao = arquivo.substring(arquivo.lastIndexOf("."), arquivo.length());
+        }catch(Exception e){
+        }
+        return extensao;
     }
 }
