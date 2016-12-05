0078a408bfbbe4e080ebcb3313befec9686e1755
Eduardo <eduardo.dornel@hotmail.com>
2016-12-05 10:53:33 -0200

    refactor(loginMBean): adicionar condição em login

--- src/CRESCER/modulo-08-java/aula5-carlos/src/main/java/br/com/cwi/crescer/aula5/mbean/LoginMBean.java
+++ src/CRESCER/modulo-08-java/aula5-carlos/src/main/java/br/com/cwi/crescer/aula5/mbean/LoginMBean.java
@@ -1,14 +1,19 @@
 package br.com.cwi.crescer.aula5.mbean;
 
+import br.com.cwi.crescer.aula5.bean.UserBean;
+import br.com.cwi.crescer.aula5.dao.UserDao;
+import br.com.cwi.crescer.aula5.entity.Usuario;
 import static br.com.cwi.crescer.aula5.utils.UserDetails.USER_AUTH;
 
 import br.com.cwi.crescer.aula5.filters.User;
 import br.com.cwi.crescer.aula5.utils.FacesUtils;
 import br.com.cwi.crescer.aula5.utils.UserDetails;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
 
 /**
  * @author Carlos H. Nonnemacher
@@ -39,12 +44,32 @@ public class LoginMBean implements Serializable {
     }
 
     public String login() {
-        FacesUtils.getSession().setAttribute(USER_AUTH, this.user);
-        return "index";
+        if (existeUsuario(user)) {
+            FacesUtils.getSession().setAttribute(USER_AUTH, this.user);
+            return "index";
+        } else {
+            return "login";
+        }
     }
 
     public String logout() {
         FacesUtils.getSession().invalidate();
         return "login";
     }
+
+    private List<Usuario> listarUsuariosExistentes() {
+        return new UserBean().findAll();
+    }
+
+    private boolean existeUsuario(User usuario) {
+        List<Usuario> usuariosCadastrados = listarUsuariosExistentes();
+        for (Usuario u : usuariosCadastrados) {
+            if (u.getEmail().equals(usuario.getEmail()) && u.getSenha().equals(usuario.getPassword())) {
+                return true;
+            }
+        }
+        return false;
+        //return usuariosCadastrados.stream().anyMatch((u) -> (u.getEmail().equals(usuario.getEmail()) && u.getSenha().equals(usuario.getPassword())));
+    }
+
 }
